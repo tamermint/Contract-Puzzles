0d9f66b2a6459f3dcb6004ac2e06d86f692af738
Vivek <vivek.mitra@outlook.com>
2025-02-03 20:42:02 +1100

    puzzles completed

--- test/game3Test.js
+++ test/game3Test.js
@@ -1,35 +1,43 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game3', function () {
+describe("Game3", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game3');
+    const Game = await ethers.getContractFactory("Game3");
     const game = await Game.deploy();
 
     // Hardhat will create 10 accounts for you by default
     // you can get one of this accounts with ethers.provider.getSigner
     // and passing in the zero-based indexed of the signer you want:
     const signer = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(2);
+    const signer3 = ethers.provider.getSigner(3);
 
     // you can get that signer's address via .getAddress()
     // this variable is NOT used for Contract 3, just here as an example
     const address = await signer.getAddress();
 
-    return { game, signer };
+    return { game, signer, signer2, signer3 };
   }
 
-  it('should be a winner', async function () {
-    const { game, signer } = await loadFixture(deployContractAndSetVariables);
-
+  it("should be a winner", async function () {
+    const { game, signer, signer2, signer3 } = await loadFixture(
+      deployContractAndSetVariables
+    );
+    const address1 = await signer.getAddress();
+    const address2 = await signer2.getAddress();
+    const address3 = await signer3.getAddress();
     // you'll need to update the `balances` mapping to win this stage
 
     // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: '1' });
+    await game.connect(signer).buy({ value: "2" });
+    await game.connect(signer2).buy({ value: "3" });
+    await game.connect(signer3).buy({ value: "1" });
 
     // TODO: win expects three arguments
-    await game.win();
+    await game.win(address1, address2, address3);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
--- test/game4Test.js
+++ test/game4Test.js
@@ -1,21 +1,34 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Game4', function () {
+describe("Game4", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
+    const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
+    const signer = ethers.provider.getSigner(0);
+    const signer1 = ethers.provider.getSigner(1);
+    const signer2 = ethers.provider.getSigner(2);
 
-    return { game };
+    return { game, signer, signer1, signer2 };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, signer, signer1, signer2 } = await loadFixture(
+      deployContractAndSetVariables
+    );
 
     // nested mappings are rough :}
 
-    await game.win();
+    const addr1 = await signer.getAddress();
+    const addr2 = await signer1.getAddress();
+    const addr3 = await signer2.getAddress();
+
+    await game.connect(signer).write(addr2);
+    await game.connect(signer2).write(addr1);
+
+    await game.connect(signer).win(addr3);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
